04068055a7445e93da3e128a7c2911c9cae2e4d5
SAMANTA LEITE <samanta.leite@fatec.sp.gov.br>
2024-03-18 12:34:08 -0300

    Aula 1803 - Validações Restantes

--- StockApp/StockApp.Domain/Entities/Product.cs
+++ StockApp/StockApp.Domain/Entities/Product.cs
@@ -39,6 +39,12 @@ namespace StockApp.Domain.Entities
             DomainExceptionValidation.When(name.Length < 5,
                 "Invalid description, too short, minimum 5 characters.");
 
+            DomainExceptionValidation.When(price < 0, "Invalid price negative value.");
+
+            DomainExceptionValidation.When(stock < 0, "Invalid stock negative value.");
+
+            DomainExceptionValidation.When(image.Length > 250, "Invalid image name too long, maximum 250 characters.");
+
         }
     }
 }
