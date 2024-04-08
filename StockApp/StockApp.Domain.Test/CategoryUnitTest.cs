d40e2b7fade167e87953ceda14511a4f46e12fe4
ANA ALMEIDA <ana.almeida57@fatec.sp.gov.br>
2024-04-08 12:30:58 -0300

    Aula 0804

---
+++ StockApp/StockApp.Domain.Test/CategoryUnitTest.cs
@@ -0,0 +1,44 @@
+using FluentAssertions;
+using StockApp.Domain.Entities;
+
+namespace StockApp.Domain.Test
+{
+    #region Testes positivos
+    public class CategoryUnitTest
+    {
+        [Fact(DisplayName ="Create Category with Valid State")]
+        public void CreateCategory_WithValidParameters_ResultValidState()
+        {
+            Action action = () => new Category(1, "Category Name");
+            action.Should().NotThrow<StockApp.Domain.Validation.DomainExceptionValidation>();
+        }
+
+        #endregion
+
+        #region Testes negativos
+        [Fact(DisplayName = "Create Category with Invalid State Id")]
+        public void CreateCategory_WithInvalidParameters_DomainExceptionInValidId()
+        {
+            Action action = () => new Category(-1, "Category Name");
+            action.Should().Throw<StockApp.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid Id value.");
+        }
+
+        [Fact(DisplayName = "Create Category with Invalid State Name")]
+        public void CreateCategory_WithInvalidParameters_DomainExceptionInValidName()
+        {
+            Action action = () => new Category(1, "Ca");
+            action.Should().Throw<StockApp.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name, too short, minimum 3 characters.");
+        }
+
+        [Fact(DisplayName = "Create Category with Null State Name")]
+        public void CreateCategory_WithNullParameters_DomainExceptionInValidName()
+        {
+            Action action = () => new Category(1, null);
+            action.Should().Throw<StockApp.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name, name is required.");
+        }
+        #endregion
+    }
+}
